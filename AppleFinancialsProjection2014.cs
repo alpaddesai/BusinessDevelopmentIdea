ea916984707f839076d3fe1412416b2d3f1ae901
Alpa D. Desai <50643387+alpaddesai@users.noreply.github.com>
2019-05-17 16:39:22 -0400

    Add files via upload

---
+++ AppleFinancialsProjection2014.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+
+
+public class AppleFinancialsProjection2014: PrivateFunctionality  // inherits all functions/methods
+{
+    
+ 
+    public double ApplePricePerShare {get; set; }
+    public double valuationAppleVariable { get; set; }
+    public double PioneerPricePerShare { get; set; }
+    public double valuationPioneerVariable { get; set; }
+
+    public double combinedEntityPricerPerShare { get; set; }
+    public double valuationVariable { get; set; }
+
+    public double marketSynergies { get; set; }
+    public double OperationalSynergies { get; set; }
+
+
+
+
+
+    public AppleFinancialsProjection2014()
+    {
+    }
+
+    public double AlpaAppleProprietaryValuation(double discountRate)
+    {
+
+
+        // values are in currency Dollar millions private variables
+        double ProprietarFreeCashFlow2013 = 32879;
+        double ProprietarFreeCashFlow2014 = 41287;
+        double ProprietarFreeCashFlow2015 = 44917;
+        double ProprietarFreeCashFlow2016 = 48806;
+        double ProprietarFreeCashFlow2017 = 52959;
+        double ProprietarFreeCashFlow2018 = 57377;
+        double TerminalValue = 928550;
+        double ProprietaryDiscountRate = discountRate;
+        double PricePerShare=0;
+
+        double APV = executeValuation(ProprietarFreeCashFlow2013, ProprietarFreeCashFlow2014, ProprietarFreeCashFlow2015, ProprietarFreeCashFlow2016, ProprietarFreeCashFlow2017, ProprietarFreeCashFlow2018, TerminalValue, discountRate);
+
+        double entityValuation = executePricePerShare(APV, AppleTaxShields, ApplelongTermDebt, AppleSharesOutstanding, ref PricePerShare);
+
+        ApplePricePerShare = PricePerShare;
+        return entityValuation;
+    }
+
+    //*******************************************************************************/
+
+
+    public double AlpaPioneerProprietaryValuation(double discountRate)
+    {
+
+
+        // values are in currency Yen millions private variables
+        double ProprietaryFreeCashFlow2013 = -16347;
+        double ProprietaryFreeCashFlow2014 = 1714;
+        double ProprietaryFreeCashFlow2015 = 8077;
+        double ProprietaryFreeCashFlow2016 = 14886;
+        double ProprietaryFreeCashFlow2017 = 22164;
+        double ProprietaryFreeCashFlow2018 = 29940;
+        double TerminalValue = 233436;
+        double PricePerShare=0;
+
+        double APV = executeValuation(ProprietaryFreeCashFlow2013, ProprietaryFreeCashFlow2014, ProprietaryFreeCashFlow2015, ProprietaryFreeCashFlow2016, ProprietaryFreeCashFlow2017, ProprietaryFreeCashFlow2018, TerminalValue, discountRate);
+
+        double entityValuation = executePricePerShare(APV, PioneerTaxShields, PioneerlongTermDebt, PioneerSharesOutstanding,  ref PricePerShare);
+
+        PioneerPricePerShare = PricePerShare;
+
+        return entityValuation;
+    }
+
+    //*******************************************************************************/
+
+
+    public double AlpaSynergiesProprietaryValuation(double discountRate, double MarketSynergyIncreaseinNOPAT, double OperationalSynergyDecreaseinCost)
+    {
+
+
+        // values in $USD, millions, private variables
+        double ProprietaryFreeCashFlow2013 = 32880;
+        double ProprietaryFreeCashFlow2014 = 41287;
+        double ProprietaryFreeCashFlow2015 = 44917;
+        double ProprietaryFreeCashFlow2016 = 48806;
+        double ProprietaryFreeCashFlow2017 = 52959;
+        double ProprietaryFreeCashFlow2018 = 57377;
+        double TerminalValue = 928550;
+ 
+
+        double APV = executeValuation(ProprietaryFreeCashFlow2013, ProprietaryFreeCashFlow2014, ProprietaryFreeCashFlow2015, ProprietaryFreeCashFlow2016, ProprietaryFreeCashFlow2017, ProprietaryFreeCashFlow2018, TerminalValue, discountRate);
+
+
+        marketSynergies = executeMarketSynergies(discountRate, MarketSynergyIncreaseinNOPAT);
+
+        OperationalSynergies = executeOperationalSynergies(discountRate, OperationalSynergyDecreaseinCost);
+
+        double entityValuation = APV + marketSynergies + OperationalSynergies + CombinedTaxShields;
+
+        combinedEntityPricerPerShare = Math.Round(entityValuation / CombinedSharesOutstanding);
+
+        return entityValuation;
+
+
+    }
+
+    public int printInformation(AppleFinancialsProjection2014 analysis, double currencyExchangeRate)
+    {
+        Console.WriteLine($"\n Synergy Analysis with Apple Financials Projected from 2014 onwards: \n");
+
+
+
+        Console.WriteLine($" Dashboard value of Apple Valuation is  $USD {analysis.valuationAppleVariable} million and price per share is ${analysis.ApplePricePerShare}");
+        Console.WriteLine($" Dashboard value of Pioneer Valuation  is  Yen {analysis.valuationPioneerVariable} million and price per share is Yen {analysis.PioneerPricePerShare}");
+        Console.WriteLine($" Dashboard value of Pioneer Valuation is  $USD {Math.Round((analysis.valuationPioneerVariable * currencyExchangeRate), 2)} million and price per share is $ {Math.Round(((analysis.PioneerPricePerShare) * currencyExchangeRate), 2)}");
+
+        Console.WriteLine($" Dashboard value of Combined Entity Valuation  is  $USD {analysis.valuationVariable} million and price per share is $USD {analysis.combinedEntityPricerPerShare} with ");
+        Console.WriteLine($" Market Synergies of $USD {analysis.marketSynergies} million and  Operational Synergies of $USD {analysis.OperationalSynergies} million     ");
+
+        Console.ReadKey();
+
+        return 0;
+    }
+
+
+
+
+    //************************ ENCAPSULATED INTERFACE NOT ACCESSIBLE TO THE USER ***********************************/
+
+}  // end of class ApplesFinancialProjections
---
+++ PrivateFunctionality.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+public class PrivateFunctionality
+{
+
+    //************* PRIVATE FUNCTION EXECUTE OPERATIONAL SYNERGIES ************************/
+
+    protected static double AppleTaxShields = 12140;
+    protected static int AppleSharesOutstanding = 5865;
+    protected static double ApplelongTermDebt = 16960;
+
+    protected static double PioneerTaxShields = 17550;
+    protected static int PioneerSharesOutstanding = 367;  // financial transaction includes selling Pioneer equity
+    protected static double PioneerlongTermDebt = 15078;
+
+    protected static double CombinedTaxShields = 12287;
+    protected static int CombinedSharesOutstanding = 5865;  // Price per share will increase due to synergies
+    protected static double CombinedlongTermDebt = 16960;   // raising capital through Pioneer equity transactions
+
+
+    public PrivateFunctionality()
+    {
+
+    }
+
+
+    protected double executeOperationalSynergies(double discountRate, double OperationalSynergyDecreaseinCost)
+    {
+        double custompegRate = 0.0920;
+        double ProprietaryFreeCashFlow2014 = 41287 * OperationalSynergyDecreaseinCost;
+        double ProprietaryFreeCashFlow2015 = ProprietaryFreeCashFlow2014 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2016 = ProprietaryFreeCashFlow2015 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2017 = ProprietaryFreeCashFlow2016 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2018 = ProprietaryFreeCashFlow2017 * (1 + custompegRate);
+        double TerminalValue = (ProprietaryFreeCashFlow2018 * (1 + 0.05)) / (discountRate - 0.05);
+
+        double APV = Math.Round(ProprietaryFreeCashFlow2014 / (Math.Pow((1 + discountRate), 1)) +
+                                 ProprietaryFreeCashFlow2015 / (Math.Pow((1 + discountRate), 2)) +
+                                 ProprietaryFreeCashFlow2016 / (Math.Pow((1 + discountRate), 3)) +
+                                 ProprietaryFreeCashFlow2017 / (Math.Pow((1 + discountRate), 4)) +
+                                 (ProprietaryFreeCashFlow2018 + TerminalValue) / (Math.Pow((1 + discountRate), 5)), 2);
+        return APV;
+    }
+
+
+
+    //************* PRIVATE FUNCTION EXECUTE MARKET SYNERGIES ************************/
+
+    protected double executeMarketSynergies(double discountRate, double MarketSynergyIncreaseinNOPAT)
+    {
+        double custompegRate = 0.0920;
+        double ProprietaryFreeCashFlow2014 = 43740 * MarketSynergyIncreaseinNOPAT;
+        double ProprietaryFreeCashFlow2015 = ProprietaryFreeCashFlow2014 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2016 = ProprietaryFreeCashFlow2015 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2017 = ProprietaryFreeCashFlow2016 * (1 + custompegRate);
+        double ProprietaryFreeCashFlow2018 = ProprietaryFreeCashFlow2017 * (1 + custompegRate);
+        double TerminalValue = (ProprietaryFreeCashFlow2018 * (1 + 0.05)) / (discountRate - 0.05);
+
+        double APV = Math.Round(ProprietaryFreeCashFlow2014 / (Math.Pow((1 + discountRate), 1)) +
+                                 ProprietaryFreeCashFlow2015 / (Math.Pow((1 + discountRate), 2)) +
+                                 ProprietaryFreeCashFlow2016 / (Math.Pow((1 + discountRate), 3)) +
+                                 ProprietaryFreeCashFlow2017 / (Math.Pow((1 + discountRate), 4)) +
+                                 (ProprietaryFreeCashFlow2018 + TerminalValue) / (Math.Pow((1 + discountRate), 5)), 2);
+        return APV;
+    }
+
+
+    //************* PRIVATE FUNCTION EXECUTE PRICE PER SHARE ************************/
+    protected double executePricePerShare(double APV, double taxShields, double longtermDebt, double SharesOutstanding, ref double pricePerShare)
+    {
+
+        double entityValuation = APV + taxShields;
+        double equityValuation = entityValuation - longtermDebt;
+
+        pricePerShare = Math.Round(equityValuation / SharesOutstanding, 2);
+
+        return entityValuation;
+    }
+
+    //******************* PRIVATE FUNCTION EXECUTE VALIDATION *********************/
+    protected double executeValuation(double ProprietaryFreeCashFlow2013, double ProprietaryFreeCashFlow2014, double ProprietaryFreeCashFlow2015, double ProprietaryFreeCashFlow2016, double ProprietaryFreeCashFlow2017, double ProprietaryFreeCashFlow2018, double TerminalValue, double ProprietaryDiscountRate)
+    {
+        double APV = Math.Round(ProprietaryFreeCashFlow2013 / (Math.Pow((1 + ProprietaryDiscountRate), 1)) +
+                                    ProprietaryFreeCashFlow2014 / (Math.Pow((1 + ProprietaryDiscountRate), 2)) +
+                                    ProprietaryFreeCashFlow2015 / (Math.Pow((1 + ProprietaryDiscountRate), 3)) +
+                                    ProprietaryFreeCashFlow2016 / (Math.Pow((1 + ProprietaryDiscountRate), 4)) +
+                                    ProprietaryFreeCashFlow2017 / (Math.Pow((1 + ProprietaryDiscountRate), 5)) +
+                                    (ProprietaryFreeCashFlow2018 + TerminalValue) / (Math.Pow((1 + ProprietaryDiscountRate), 6)), 2);
+        return APV;
+    }
+
+
+    //*******************************************************************************/
+
+}
+
+
